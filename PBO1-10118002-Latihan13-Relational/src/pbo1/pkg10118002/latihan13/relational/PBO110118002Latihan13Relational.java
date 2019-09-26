7476db7333657ade3fad50b27419bd23cdcc02f8
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-09-26 15:42:28 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan13-Relational/src/pbo1/pkg10118002/latihan13/relational/PBO110118002Latihan13Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan13.relational;
+
+/**
+ *
+ * @author ASUS
+ * Nama     : Rizky Muslimin
+ * Kelas    : IF1
+ * NIM      : 10118002
+ */
+public class PBO110118002Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int bil1 = 10;
+    int bil2 = 20;
+        System.out.println("a == b = "+(bil1 == bil2));
+        System.out.println("a != b = "+ (bil1 != bil2));
+        System.out.println("a > b = "+ (bil1 > bil2));
+        System.out.println("a < b = "+ (bil1 < bil2));
+        System.out.println("b >= a = "+ (bil2 >= bil1));
+        System.out.println("b <= a = "+ (bil2 <= bil1));
+    }
+    
+}
